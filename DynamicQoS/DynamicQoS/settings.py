6e7adc931b8986c7b93d0ae38e500d7a18cfa002
Hamidabdou <boss199635->
2019-07-08 17:47:21 +0000

    edited db connection in settings

--- DynamicQoS/DynamicQoS/settings.py
+++ DynamicQoS/DynamicQoS/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-from mongoengine import * 
+from mongoengine import *
+import mongoengine
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -85,7 +86,8 @@ DATABASES = {
     }
 }
 
-connect("monitordb","0.0.0.0", port = 27017)
+
+mongoengine.connect('monitordb', host='127.0.0.1',port = 27017)
 
 
 # Password validation
