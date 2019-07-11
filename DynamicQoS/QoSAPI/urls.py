b6d9e65ae0f9e6218a2cec06614c8eefabf7f183
charefdz1995 <ccherrad@gmail.com>
2019-07-11 15:38:17 +0100

    Adapting the utils

--- DynamicQoS/QoSAPI/urls.py
+++ DynamicQoS/QoSAPI/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path 
-from .views import AddDevice
+from .views import *
 urlpatterns = [
     path('add',AddDevice.as_view(), name = "add-device"),
+    path('devices',DeviceList.as_view(), name = "device-list"),
 ]
 app_name = 'QoSAPI'
--- DynamicQoS/QoSAPI/views.py
+++ DynamicQoS/QoSAPI/views.py
@@ -1,11 +1,30 @@
 from django.shortcuts import render
 from rest_framework_mongoengine import generics
 from rest_framework import generics
-from .serializers import deviceSerializer
+from .serializers import deviceSerializer,deviceListSerializer
 from QoSmonitor.models import * 
+from napalm import get_network_driver 
+from rest_framework import status
+from rest_framework.response import Response
+
+
 
 class AddDevice(generics.CreateAPIView):
 	serializer_class = deviceSerializer
+	
+	def perform_create(self, serializer):
+		addr = self.request.data.get("management.management_address")
+		user = self.request.data.get("management.username")
+		passwd = self.request.data.get("management.password")
+		driver = get_network_driver("ios")
+		device = driver(addr,user,passwd)
+		device.open()
+		serializer.save(hostname = device.get_facts()['fqdn'])
+		device.close()
 
-
+class DeviceList(generics.ListAPIView):
+	serializer_class = deviceListSerializer
+	def get_queryset(self):
+		queryset = device.objects.all()
+		return queryset
 
