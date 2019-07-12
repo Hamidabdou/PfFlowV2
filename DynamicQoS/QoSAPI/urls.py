c5b6dbb91c4d418528531aa9ab8a383dce49647e
charefdz1995 <ccherrad@gmail.com>
2019-07-12 19:34:34 +0100

    fix api TopologyByName

--- DynamicQoS/QoSAPI/urls.py
+++ DynamicQoS/QoSAPI/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path 
 
-from .views import AddDevice, MyOwnView
+from .views import *
 
 urlpatterns = [
     path('add',AddDevice.as_view(), name = "add-device"),
-    path('topologies',MyOwnView.as_view(), name = "topologies"),
+    path('topologies',TopologyByName.as_view(), name = "topologies"),
 ]
 app_name = 'QoSAPI'
