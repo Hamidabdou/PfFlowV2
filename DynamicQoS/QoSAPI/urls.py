c44a7b0dc6572d76b3a417fa5b009773fbebec52
charefdz1995 <ccherrad@gmail.com>
2019-07-13 16:29:49 +0100

    Delete topologyList

--- DynamicQoS/QoSAPI/urls.py
+++ DynamicQoS/QoSAPI/urls.py
@@ -4,7 +4,6 @@ from .views import *
 urlpatterns = [
     path('add',AddDevice.as_view(), name = "add-device"),
     path('topology',TopologyByName.as_view(), name = "topology-by-name"),
-    path('topologies',TopologyList.as_view(), name = "topologies-list"),
     path('addtopogy', AddTopology.as_view(), name = "add-topology"), 
 ]
 app_name = 'QoSAPI'
