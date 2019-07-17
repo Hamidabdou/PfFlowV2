2f8715ee173a7644ed1dbccad3429c27c97a6c8f
charefdz1995 <ccherrad@gmail.com>
2019-07-18 00:58:26 +0100

    fix serializer

--- DynamicQoS/QoSGui/urls.py
+++ DynamicQoS/QoSGui/urls.py
@@ -6,12 +6,9 @@ from . import views
 urlpatterns = [
     path('login/', loginview.login, {'authentication_form': LoginForm}),
     path('', views.home, name='Home'),
-
     path('topologies/', views.topologies, name='Topologies'),
     path('addtopology/', views.add_topology, name='AddTopology'),
     path('drag_drop/<topo_id>', views.drag_drop, name='drag_drop'),
-
     path('saveTopology/<topo_id>', views.save_json_topology, name='SaveTopology'),
-
-
 ]
+
